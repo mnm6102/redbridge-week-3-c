8c7fec4381c52f3f46bf7b5c683581e4c162386b
Michael Arnold <44656071+mnm6102@users.noreply.github.com>
2022-09-27 16:53:17 -0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,36 +1,10 @@
 input.onButtonPressed(Button.A, function () {
     motobit.enable(MotorPower.On)
-    motobit.setMotorSpeed(Motor.Left, MotorDirection.Forward, 39)
-    motobit.setMotorSpeed(Motor.Right, MotorDirection.Forward, 0)
-    basic.showLeds(`
-        . . # . .
-        . # . . .
-        # # # # #
-        . # . . .
-        . . # . .
-        `)
-})
-input.onButtonPressed(Button.AB, function () {
-    motobit.enable(MotorPower.Off)
-    basic.showLeds(`
-        . . # . .
-        . # . # .
-        # . . . #
-        . # . # .
-        . . # . .
-        `)
+    motobit.setMotorSpeed(Motor.Left, MotorDirection.Forward, 30)
+    motobit.setMotorSpeed(Motor.Right, MotorDirection.Forward, 30)
 })
 input.onButtonPressed(Button.B, function () {
-    motobit.enable(MotorPower.On)
-    motobit.setMotorSpeed(Motor.Left, MotorDirection.Forward, 0)
-    motobit.setMotorSpeed(Motor.Right, MotorDirection.Forward, 30)
-    basic.showLeds(`
-        . . # . .
-        . . . # .
-        # # # # #
-        . . . # .
-        . . # . .
-        `)
+    motobit.enable(MotorPower.Off)
 })
 motobit.enable(MotorPower.Off)
 motobit.invert(Motor.Left, true)
